9a413c8465ca6e0c65eede86903ff54d07de9ee7
maikia <maja_ka@hotmail.com>
2020-11-06 13:07:34 +0100

    simplify the resample estimator to only take every 100th sample

--- submissions/knn_resample/estimator.py
+++ submissions/knn_resample/estimator.py
@@ -5,42 +5,14 @@ from sklearn.base import BaseEstimator
 from sklearn.compose import make_column_transformer
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.base import ClassifierMixin, TransformerMixin
 
 N_JOBS = 1
 
 
 class Resampler(BaseEstimator):
-    ''' Resamples X and y to 2 subjects and 500 datasamples '''
+    ''' Resamples X and y to every 100th sample '''
     def fit_resample(self, X, y):
-        X = X.reset_index(drop=True)  # make sure the indices are ordered
-        # 1. take only first 2 subjects
-        subjects_used = np.unique(X['subject'])[:2]
-        X = X.loc[X['subject'].isin(subjects_used)]
-
-        # 2. take only n_samples from each of the subjects
-        n_samples = 500  # use only 500 samples/subject
-        X = X.groupby('subject').apply(
-            lambda s: s.sample(n_samples, random_state=42))
-
-        X = X.reset_index(level=0, drop=True)  # drop subject index
-
-        # get y corresponding to chosen X_df
-        y = y[X.index]
-        return X, y
-
-
-class Checker(BaseEstimator, ClassifierMixin, TransformerMixin):
-    def fit(self, X, y):
-        print('fit')
-        print(f'x.shape:{X.shape}')
-        print(f'y.shape:{y.shape}')
-        return self
-
-    def transform(self, X):
-        print('transform')
-        print(f'x.shape:{X.shape}')
-        return X
+        return X[::100], y[::100]  # take 1% of data
 
 
 def get_estimator():
@@ -51,12 +23,9 @@ def get_estimator():
                                            remainder='passthrough')
     rus = Resampler()
 
-    checker = Checker()
     pipeline = Pipeline([
         ('downsampler', rus),
-        ('checker', checker),
         ('transformer', preprocessor),
-        # ('checker2', checker),
         ('classifier', kneighbors)
 
         ])
