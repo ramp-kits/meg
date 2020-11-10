1427a2a43852f1f5712eede66cb6e67e04dcb9df
EC2 Default User <ec2-user@ip-172-31-12-40.eu-west-1.compute.internal>
2020-11-10 14:08:47 +0000

    added algorithm brute for less memory usage

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -4,13 +4,13 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.pipeline import Pipeline
 
 
-N_JOBS = -1
+N_JOBS = 1
 
 
 def get_estimator():
 
     # K-nearest neighbors
-    clf = KNeighborsClassifier(n_neighbors=3)
+    clf = KNeighborsClassifier(n_neighbors=3,algorithm='brute')
     kneighbors = MultiOutputClassifier(clf, n_jobs=N_JOBS)
 
     preprocessor = make_column_transformer(('drop', ['subject', 'L_path']),
