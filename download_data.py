024ab85b284b4dc7b83b27bf8e6d971c97b9f4b5
maikia <maja_ka@hotmail.com>
2020-11-20 13:20:04 +0100

    enabled option to either download the public dataset (default) or a private one if the username and password to the osf account are provided

--- download_data.py
+++ download_data.py
@@ -1,3 +1,4 @@
+import click
 import os
 from osfclient.api import OSF
 
@@ -15,20 +16,37 @@ from osfclient.api import OSF
 LOCAL_PATH = 'data'  # local path to the data
 REMOTE_PATH = 'MEG/'  # remote path where to store the data on OSF
 PROJECT_CODE = 't4uf8'  # to find your PROJECT_CODE navigate to your OSF
+PROJECT_CODE_PRIVATE = 'vw8sh'
 # project on the web. The link will be something of this type:
 # https://osf.io/t4uf8/ , here t4uf8 is the PROJECT_CODE
 
-# if the file already exists it will overwrite it
-# osf = OSF(username=USERNAME, password=PASSWORD)
-osf = OSF()
-project = osf.project(PROJECT_CODE)
 
-destination = 'https://osf.io/' + PROJECT_CODE + '/'
-store = project.storage('osfstorage')
-
-
-def download_from_osf():
+@click.command()
+@click.option("--private", is_flag=True)
+@click.option(
+    "--username",
+    help="Your username to the private repository"
+)
+@click.option(
+    "--password",
+    help="Your password to the private repository"
+)
+def download_from_osf(private, username, password):
     file_idx = 0
+    if private:
+        project_code = PROJECT_CODE_PRIVATE
+    else:
+        project_code = PROJECT_CODE
+
+    # if the file already exists it will overwrite it
+    # osf = OSF(username=USERNAME, password=PASSWORD)
+    if private:
+        osf = OSF(username=username, password=password)
+    else:
+        osf = OSF()
+    project = osf.project(project_code)
+    store = project.storage('osfstorage')
+
     for file_ in store.files:
         # get only those files which are stored in REMOTE_PATH
         pathname = file_.path
